9e8fc5a26b8275842fca78ea1aeeb4a1439dcace
valentinvisan1288 <valentinvisan1288@gmail.com>
2022-07-31 10:16:40 +0200

    New PC

--- portfolioapp/palindrome/script.js
+++ portfolioapp/palindrome/script.js
@@ -1,30 +1,55 @@
-function palindromChecker(txt) {
-  let reversed = txt.split("").reverse().join("");
-  if (reversed === txt) {
+"use strict";
+const palindromChecker = function (txt) {
+  //txt.toLowerCase(); //Check for upper and lower case and symbols
+  const txtLower = txt.toLowerCase();
+  let reversed = txtLower.split("").reverse().join("");
+  reversed;
+  if (reversed === txtLower) {
     console.log(
-      `"${txt}" spelled backwards is "${reversed}", so it is a palindrome`
+      `"${txtLower}" spelled backwards is "${reversed}", so it is a palindrome`
     );
-    document
-      .querySelector(".result")
-      .setAttribute(
-        `"${txt}" spelled backwards is "${reversed}", so it is a palindrome`
-      );
+    // document
+    //   .querySelector(".result")
+    //   .setAttribute(
+    //     `"${txt}" spelled backwards is "${reversed}", so it is a palindrome`
+    //   );
     return true;
   } else {
-    document
-      .querySelector(".result")
-      .setAttribute(
-        `"${txt}" is not a palindrome because, spelled backwards it is"${reversed}".`
-      );
     console.log(
-      `"${txt}" is not a palindrome because, spelled backwards it is"${reversed}".`
+      `"${txtLower}" is not a palindrome because, spelled backwards it is "${reversed}".`
     );
+    // document
+    //   .querySelector(".result")
+    //   .setAttribute(
+    //     `"${txt}" is not a palindrome because, spelled backwards it is"${reversed}".`
+    //   );
   }
   return false;
-}
+};
 
-console.log(palindromChecker("eye"));
+palindromChecker("Bob e bob");
 
 // if ((palindromChecker = true)) {
 //   document.querySelector("modalwindow").class("hidden");
 // }
+const palindrom = [];
+let newArray = [];
+function paliCheckr(palindrom) {
+  for (let i = palindrom.length - 1; i >= 0; i--) {
+    console.log(palindrom[i]);
+    newArray = new Array.push[palindrom[i]]();
+
+    // let [] = [...palindrom];
+  }
+  console.log(newArray);
+}
+paliCheckr("bob");
+
+// String toBeChecked; //cuvantul nostru
+
+// toBeChecked > array
+// int sizeOfPalindrom = toBeChecked.length;
+
+// // for(i = 0; i < sizeOfPalindrom; i++)
+// iteram prin toate pozitiile array-ul
+// pozitia 0 o adaugam in alt array pe pozitia sizeofPalindrom;
